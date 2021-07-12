701f0d77414d0ec8f0dc97bc04f1150956b57a3e
halki <s.halki0203@icloud.com>
2021-07-12 22:46:42 +0900

    crud実装

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/','ListingsController@index');
+
+//リスト新規画面
+Route::get('/new', 'ListingsController@new')->name('new');
+
+//リスト新規処理
+Route::post('/listings','ListingsController@store');
+
+//リスト更新画面
+Route::get('/listingsedit/{listing_id}', 'ListingsController@edit');
+
+//リスト更新処理
+Route::post('/listing/edit','ListingsController@update');
+
+//リスト削除処理
+Route::get('/listingsdelete/{listing_id}', 'ListingsController@destroy');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
